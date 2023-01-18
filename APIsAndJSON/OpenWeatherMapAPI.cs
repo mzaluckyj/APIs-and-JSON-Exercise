91302cf685a0e6d1d93dce81fdb7f5af595efc18
Michael <m.zaluckyj@gmail.com>
2023-01-18 12:02:05 -0500

    exercise 2

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+using System.Net.Http;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var repsone = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(repsone)["main"]["temp"].ToString());
+
+            return temp;
+
+        }
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -9,6 +9,9 @@ namespace APIsAndJSON
         {
 
             //exercise 1
+
+            Console.WriteLine("Exercise 1");
+
             for(int x =0; x< 5; x++)
             {
                 RonVSKanyeAPI.KWQoute();
@@ -16,6 +19,20 @@ namespace APIsAndJSON
             }
 
             //exercise 2
+            Console.WriteLine("Exercise 2");
+
+            string Key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(Key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please eneter your Zipcode");
+            var zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+            Console.WriteLine("");
+
+            Console.WriteLine($"It is currently {OpenWeatherMapAPI.GetTemp(apiCall)} degrees F in your location");
+
+            
         }
     }
 }
