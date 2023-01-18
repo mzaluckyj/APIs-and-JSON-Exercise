3decb09d28fd2b40d4afc3fdbb04726e8fc014d5
Michael <m.zaluckyj@gmail.com>
2023-01-18 10:34:55 -0500

    exercise 1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //exercise 1
+            for(int x =0; x< 5; x++)
+            {
+                RonVSKanyeAPI.KWQoute();
+                RonVSKanyeAPI.RSQoute();
+            }
+
+            //exercise 2
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        public static void KWQoute()
+        {
+            var client = new HttpClient();
+
+            var KWURL = "https://api.kanye.rest/";
+
+            var KWResponse = client.GetStringAsync(KWURL).Result;
+
+            var KWestQuote = JObject.Parse(KWResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye:   {KWestQuote}");
+            Console.WriteLine("");
+
+        }
+
+        public static void RSQoute()
+        {
+            var client = new HttpClient();
+
+            var RSURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var RSResponse = client.GetStringAsync(RSURL).Result;
+
+            var RonSQuote = JArray.Parse(RSResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+
+            Console.WriteLine($"Ron:   {RonSQuote}");
+            Console.WriteLine("");
+        }
+
     }
 }
